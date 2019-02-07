d58c38cfb7d3ae6a7ffb7fea205487d9eeb0bf52
David Alejandro Marin Alzate <david.marin@sofka.com.co>
2019-02-07 08:56:08 -0500

    add unit test for the userRepository

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,19 +2,26 @@ package com.example.demo;
 
 import com.example.demo.proyecto.model.User;
 import com.example.demo.proyecto.repositorio.UserRepository;
+import org.bson.types.ObjectId;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.Assert;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
 
-	private final String  URL_API= "http://localhost/rest/user";
+	private final String  URL_API= "http://localhost:8095/rest/user";
 
 	@Autowired
 	private UserRepository userRepository;
@@ -29,12 +36,58 @@ public class DemoApplicationTests {
 	}
 
 	@Test(timeout=3000)
-	public void testPostRides(){
+	public void testGetUsers(){
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<List<User>> responseEntity =
+				restTemplate.exchange(
+						URL_API + "/",
+						HttpMethod.GET ,
+						null,
+						new ParameterizedTypeReference<List<User>>(){});
+		List<User> users = responseEntity.getBody();
+		Assert.noNullElements(users.toArray(),"no es null");
+
+}
+
+	@Test(timeout=3000)
+	public void testGetUser(){
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<User> responseEntity = restTemplate.getForEntity(URL_API+"/5c5c2e3a73697c1598d9b0dd",User.class);
+	}
+
+	@Test(timeout=3000)
+	public void testPostUser(){
 		RestTemplate restTemplate = new RestTemplate();
 
-		User user = new User(45, "Migue", "3004334345");
-		Object userResponse = new Object();
-		restTemplate.put(URL_API+"/",user);
+		User user = new User();
+		user.setId(new ObjectId("5c5c362673697c2d2053c2fc"));
+		user.setName("Pedro Picapiedra");
+		user.setTelephone("8544977");
+
+		ResponseEntity<User> responseEntity = restTemplate.postForEntity(URL_API+"/", user, User.class);
+		Assert.notNull(responseEntity.getBody(),"Body is no null");
+		Assert.isTrue(responseEntity.getStatusCode() == HttpStatus.OK,"Status Code");
 	}
+
+	@Test(timeout=3000)
+	public void testPutUser(){
+		RestTemplate restTemplate = new RestTemplate();
+		String ID = "5c5c362673697c2d2053c2fc";
+
+		User user = new User();
+		user.setId(new ObjectId());
+		user.setName("Pedro Pablo");
+		user.setTelephone("8544977");
+
+		restTemplate.put(URL_API + "/" + ID, user, User.class);
+	}
+
+	@Test(timeout=3000)
+	public void testDeleteUser(){
+		RestTemplate restTemplate = new RestTemplate();
+		restTemplate.delete(URL_API+"/5c5c362673697c2d2053c2fc",User.class);
+	}
+
+
 }
 
